8a20c7d9a97ff5e200d76275096fcf098f5b3747
kinchungwong <kinchungwong@users.noreply.github.com>
2025-05-24 11:27:42 -0700

    hash functions needed for impl (not tested)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
-int main(int argc, char** argv) {
+#include "common/project_macros.hpp"
+#include "tg/data/hashing/superfasthash_detail.hpp"
+#include "tg/data/hashing/fnv1a_detail.hpp"
+#include "tg/data/hashing/bitwise_detail.hpp"
+
+int main(int argc, char** argv)
+{
     return 0;
 }
